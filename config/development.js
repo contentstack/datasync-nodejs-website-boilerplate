6d19e23952c4680bf1bc6f0ec338f6aab9f0cb9c
Abhinav Gupta <abhinav.gupta@contentstack.com>
2022-06-22 11:37:48 +0530

    fix: fixed issue cs-25622

--- config/development.js
+++ config/development.js
@@ -1,3 +1,14 @@
+/* 
+  if the master language of the stack is something other than en-us,
+  it will have to be added in the contentStore config
+
+  example, if master language of a stack is 'ru-ru'
+
+  contentStore : {
+    baseDir: './_contents',
+    locale: 'ru-ru'
+  }
+*/
 const config = {
   sdk: 'datasync-filesystem-sdk',
   contentstack: {
